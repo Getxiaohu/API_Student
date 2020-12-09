1282b91378d5709113e325c79d556203d5f87fff
zhq1211 <2982957828@qq.com>
2020-12-09 11:15:50 +0800

    12316456

---
+++ API_Student/Controllers/ihfeqhfhController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace API_Student.Controllers
+{
+    public class ihfeqhfhController : Controller
+    {
+        // GET: ihfeqhfh
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
